14e6e37580ddb9eecac1910423304775f3d866f2
Felix Marinho <felixmark3@gmail.com>
2025-03-18 22:31:36 -0300

    Aula sobre condições

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 79.3;
+console.log(`A velocidad do seu carro é ${vel}Km/h.`);
+if (vel > 60) {
+  //CONDIÇÃO SIMPLES
+  console.log(`Você ultrapassou a velocidad permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando cinto de segurança.`);
